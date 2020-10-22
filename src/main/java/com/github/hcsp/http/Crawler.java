19e1064ff63911be4dfecd101f8eaedc74e6c6cf
Administrator <619805205@qq.com>
2020-10-23 01:06:52 +0800

    优化代码

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -30,6 +30,15 @@ public class Crawler {
             this.title = title;
             this.author = author;
         }
+
+        @Override
+        public String toString() {
+            return "GitHubPullRequest{" +
+                    "number=" + number +
+                    ", title='" + title + '\'' +
+                    ", author='" + author + '\'' +
+                    '}';
+        }
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
@@ -59,15 +68,12 @@ public class Crawler {
             };
             String responseBody = httpclient.execute(httpget, responseHandler);
 
-
-            List<String> list = JSON.parseArray(responseBody, String.class);
-            for (String item : list
+            List<JSONObject> list = JSON.parseArray(responseBody, JSONObject.class);
+            for (JSONObject item : list
             ) {
-                JSONObject obj = JSON.parseObject(item);
-                int number = (int) obj.get("number");
-                String title = obj.get("title").toString();
-                JSONObject user = JSON.parseObject(obj.get("user").toString());
-                String author = user.get("login").toString();
+                int number = item.getInteger("number");
+                String title = item.getString("title");
+                String author = item.getJSONObject("user").getString("login");
                 target.add(new GitHubPullRequest(number, title, author));
             }
             return target;
